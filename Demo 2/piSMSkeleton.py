f47662345a889b9db9c871b88eba35aac826d5a9
= <=>
2023-03-31 16:17:18 -0600

    Exposure changes and such

--- Demo 2/piSMSkeleton.py
+++ Demo 2/piSMSkeleton.py
@@ -3,26 +3,6 @@ import numpy as np
 import cv2 as cv
 import cv2.aruco as aruco
 
-# Some included camera matrices for possible calibration implementation
-"""camMtx = np.array([[ 605.21881298, 0,            304.72314493],
-                   [ 0,            602.70534241, 222.09439201],
-                   [ 0,            0,            1           ]])"""
-
-camMtx = np.array([[ 605.21881298, 0,            304.72314493],
-                   [ 0,            602.70534241, 222.09439201],
-                   [ 0,            0,            1           ]])
-
-dist = np.array([[ 0, 0, 0, 0, 0 ]])
-
-"""dist = np.array([[  5.71005717e-02,  -6.19737430e-02,  -7.16729572e-03,
-  5.17895277e-04,  -7.38185549e-01]])"""
-
-# radial^2, radial^4, tangentA, tangentB, radial^6
-
-h,w = (480,640)
-newCamMtx, roi = cv.getOptimalNewCameraMatrix(camMtx,dist,(w,h),1,(w,h))
-x,y,w,h = roi
-
 
 ## I2C Imports
 import smbus
@@ -50,6 +30,7 @@ def angleMeas():
 def distMeas():
   #return move
   pass
+
 
 def I2CMessage(offset, float):
   #general I2C message to send
--- Demo 2/poseExperiments.py
+++ Demo 2/poseExperiments.py
@@ -2,44 +2,52 @@
 import numpy as np
 import cv2 as cv
 import cv2.aruco as aruco
+from time import sleep
 
 # Some included camera matrices for possible calibration implementation
 """camMtx = np.array([[ 605.21881298, 0,            304.72314493],
                    [ 0,            602.70534241, 222.09439201],
                    [ 0,            0,            1           ]])"""
 
-camMtx = np.array([[ 605.21881298, 0,            304.72314493],
-                   [ 0,            602.70534241, 222.09439201],
-                   [ 0,            0,            1           ]])
+#camMtx = np.array([[ 605.21881298, 0,            304.72314493],
+                   #[ 0,            602.70534241, 222.09439201],
+                   #[ 0,            0,            1           ]])
 
-dist = np.array([[ 0, 0, 0, 0, 0 ]])
+#dist = np.array([[ 0, 0, 0, 0, 0 ]])
 
 """dist = np.array([[  5.71005717e-02,  -6.19737430e-02,  -7.16729572e-03,
   5.17895277e-04,  -7.38185549e-01]])"""
 
 # radial^2, radial^4, tangentA, tangentB, radial^6
 
-h,w = (480,640)
-newCamMtx, roi = cv.getOptimalNewCameraMatrix(camMtx,dist,(w,h),1,(w,h))
-x,y,w,h = roi
+#h,w = (480,640)
+#newCamMtx, roi = cv.getOptimalNewCameraMatrix(camMtx,dist,(w,h),1,(w,h))
+#x,y,w,h = roi
 
 
 ## OpenCV Distance Calculation
 def distCalc(markID, corners, hfResX=320, resY=480, fovY=48):
-  markerLabel = 'Marker {} distance is {} (inches/cm/ft)\n'
+  #markerLabel = '{}, {}, {}, {}'
+  markerLabel = 'Marker {} distance is {} ft\n'
+  #markerLabel = 'Marker {} pixels from camera bottom: {}'
   yOffset = 0
   
   for ind in range(4):
     yOffset += resY - corners[ind][1]
-  dist = 0 # Some distance regression
+  yOffset = yOffset/ 4
+  dist = ((yOffset - 560)/(-636)) ** -1.234 # Some distance regression
   
-  print(markerLabel.format(markID, yOffset))
-  return yDeg
+  print(markerLabel.format(markID, dist))
+  return dist
 
 ## OpenCV Print to Camera
 
 ## OpenCV Camera Setup
 cap = cv.VideoCapture(-1)
+cap.read()
+cap.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.25)
+cap.set(cv.CAP_PROP_EXPOSURE, 0.0075)
+
 if not cap.isOpened():
   print('Err: cannot open camera\n')
   exit()
@@ -57,10 +65,10 @@ while True:
   
   # Some image transformations for feeding into the detectMarkers function
   grayed = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-  grayed = cv.undistort(grayed, camMtx, dist, None, newCamMtx)
+  #grayed = cv.undistort(grayed, camMtx, dist, None, newCamMtx)
   #grayed = grayed[y:y+h, x:x+w]
   
-  corners, markIDs, rejected = aruco.detectMarkers(grayed, arucoDict, parameters = params)
+  corners, markIDs, rejected = aruco.detectMarkers(frame, arucoDict, parameters = params)
   if markIDs is None:
     detected = False
     #print('No markers found.\n')
@@ -70,7 +78,7 @@ while True:
       dist = distCalc(markIDs[ind], corners[ind][0])
     detected = True
   
-  cv.imshow('Detected Markers', aruco.drawDetectedMarkers(grayed, corners))
+  cv.imshow('Detected Markers', aruco.drawDetectedMarkers(frame, corners))
   if cv.waitKey(1) == ord('q'):
     break
 
