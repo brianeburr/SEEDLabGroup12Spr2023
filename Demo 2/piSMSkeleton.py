d1799a243fea5b73546b22bf7c7001a64436197f
AsparObl <72943354+AsparObl@users.noreply.github.com>
2023-04-03 14:50:12 -0600

    Update piSMSkeleton.py
    Additions to state machine with comments for I2C implementation spots

--- Demo 2/piSMSkeleton.py
+++ Demo 2/piSMSkeleton.py
@@ -10,27 +10,38 @@ import board
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 from struct import *
 
-
+#####################################################################################
 ## Pi State Functions
-def idle():
-  pass
 
-def scan():
-  #if(markerFound && (angle < arbitraryThresh)):
-  #  return angleMeas
-  pass
+def angleMeas(markID, corners, hfResX=320, hFov=29.26):
+  markerLabel = 'Marker {} x-angle is {}\n'
+  xOffset = 0
+  
+  for ind in range(4):
+    xOffset += hfResX - corners[ind][0]
+  xDeg = (hFov * (xOffset / 4) / hfResX) - 0.5
+  
+  print(markerLabel.format(markID, xDeg))
+  #return xDeg
+  
+  if(-5 <= xDeg <= 5):
+    return 2, xDeg
+  else:
+    return 3, xDeg
 
-def angleMeas():
-  #if():
-  #  return adjust
-  #else:
-  #  return distMeas
-  pass
 
-def distMeas():
-  #return move
-  pass
-
+def distMeas(markID, corners, resY=480, fovY=48):
+  markerLabel = 'Marker {} distance is {} ft\n'
+  yOffset = 0
+  
+  for ind in range(4):
+    yOffset += resY - corners[ind][1]
+  yOffset = yOffset / 4
+  dist = ((yOffset - 560)/(-636)) ** -1.234 # Connor's distance regression
+  
+  print(markerLabel.format(markID, dist))
+  return dist
+#####################################################################################
 
 def I2CMessage(offset, float):
   #general I2C message to send
@@ -39,7 +50,7 @@ def I2CMessage(offset, float):
 
 ## OpenCV Camera Instantiation (Prelim)
 # Needs tweaking for different possible camera setups
-camState = idle
+camState = 0
 cap = cv.VideoCapture(-1)
 if not cap.isOpened():
   print('Err: cannot open camera\n')
@@ -56,13 +67,28 @@ while True:
     break
     
   # Image transformations for the detectMarkers function
-  #grayed = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-  #grayed = cv.undistort(grayed, camMtx, dist, None, newCamMtx)
-  #grayed = grayed[y:y+h, x:x+w]
-  #corners, markIDs, rejected = aruco.detectMarkers(grayed, arucoDict, parameters = params)
+  grayed = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+  corners, markIDs, rejected = aruco.detectMarkers(grayed, arucoDict, parameters = params)
   
-  # State machine conditions will be above
-  # I2C stuff can go here eventually
+  
+  if(camState == 0): # Idle
+    print('Idle state, no action') # Needs a short bit of code for idle state
+  elif(camState == 1): # Scan
+    if(markIDs is not None):
+      camState = 2
+      # Send stop command to robot
+      # Small delay signal to account for robot stop delay
+  elif(camState == 2): # Adjust (Camera Angle)
+    camState, angle = camState(markIDs[0], corners[0][0])
+    if(camState == 3):
+      # Send angle to robot over I2C
+    # If state is distMeas, no I2C should be needed
+  elif(camState == 3): # Distance
+    dist = camState(markID[0], corners[0][0])
+    # Send distance to Arduino
+    camState = 4
+  else: # Move
+    print('Move state, no action')
   
 print('Done.\n')
 cap.release()
