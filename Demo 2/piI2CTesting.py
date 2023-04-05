0a1ea5a75b0bbbb2bf2d5457724b2d34c8e4043c
= <=>
2023-04-05 13:51:21 -0600

    Updated testing i2c

--- Demo 2/piI2CTesting.py
+++ Demo 2/piI2CTesting.py
@@ -11,19 +11,22 @@ ardAddress = 0x04 #arduinos initialized I2C address
 def I2CMessage(offset, floatNum):
   #general I2C message to send
     #struct.unpack("f", struct.pack("f", 0.00582811585976)) might be useful if datatypes messed up
-    ba = bytearray(struct.pack("f", floatNum))
+    print(type(floatNum))
+    ba = list(bytes(struct.pack('<f', floatNum)))
+    print(ba)
     bus.write_block_data(ardAddress, offset, ba)
     pass
 
 
 def rotate(degrees):
-    I2CMessage(2, degrees)
-    I2CMessage(4,0)
+    I2CMessage(2, float(degrees))
+    
+    I2CMessage(4,0.0)
     pass
 
 def moveForward(cm):
-    I2CMessage(3, cm)
-    I2CMessage(5,0)
+    I2CMessage(3, float(cm))
+    I2CMessage(5,0.0)
     pass
 
 while(True):
