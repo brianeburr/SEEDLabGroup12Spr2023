59ba4787a61a6ba46aacfa454ffd8279fe7b83ee
= <=>
2023-04-07 10:01:16 -0600

    More tuning of camera ranges and output cleanup

--- Demo 2/piSMSkeleton.py
+++ Demo 2/piSMSkeleton.py
@@ -4,31 +4,29 @@ import cv2 as cv
 import cv2.aruco as aruco
 from enum import Enum
 from time import sleep
-import RPi.GPIO as GPIO
 
 ## I2C Imports
 import smbus
 import board
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 import struct
+import RPi.GPIO as GPIO
 
 bus = smbus.SMBus(1)
-ardAddress = 0x04 #arduinos initialized I2C address
+ardAddress = 0x04 # Arduino's initialized I2C address
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN)
 
-#####################################################################################
+################################################################################
 ## Pi State Functions
 
 angle = 0
-angleOffCounter = 0
 adjFail = False
 
 def angleMeas(markID, corners, hfResX=320, hFov=29.26):
   global adjFail
-  #angleThresh = 12 # For speed!!!
+  #angleThresh = 5 # For speed!!!
   angleThresh = 2 # For precision
-  
   markerLabel = 'Marker {} x-angle is {}\n'
   xOffset = 0
   
@@ -38,9 +36,7 @@ def angleMeas(markID, corners, hfResX=320, hFov=29.26):
   
   print(markerLabel.format(markID, xDeg))
 
-  if(abs(angle - xDeg) > 2):
-    adjFail = False
-  else:
+  if(abs(angle - xDeg) < 2):
     adjFail = True
 
   if(abs(xDeg) < angleThresh):
@@ -62,35 +58,35 @@ def distMeas(markID, corners, resY=480, fovY=48):
   
   print(markerLabel.format(markID, dist))
   return dist
-#####################################################################################
+
+################################################################################
+## I2C Comms Functions
 
 def I2CMessage(offset, floatNum):
-  #general I2C message to send
-    #struct.unpack("f", struct.pack("f", 0.00582811585976)) might be useful if datatypes messed up
-    #print(floatNum)
-    #print(offset)
-    ba = list(bytes(struct.pack('<f', floatNum)))
-
-    #print(ba)
-    bus.write_block_data(ardAddress, offset, ba)
-    pass
+  # General I2C message to send
+  #struct.unpack("f", struct.pack("f", 0.00582811585976)) might be useful if datatypes messed up
+  #print(floatNum)
+  #print(offset)
+  
+  ba = list(bytes(struct.pack('<f', floatNum)))
+  #print(ba)
+  bus.write_block_data(ardAddress, offset, ba)
 
 
 def rotate(degrees):
-    I2CMessage(2, degrees)
-    #sleep(0.1) #perhaps unnecessary
-    I2CMessage(4,0)
-    pass
+  I2CMessage(2, degrees)
+  #sleep(0.1) # Perhaps unnecessary
+  I2CMessage(4,0)
+
 
 def moveForward(cm):
-    I2CMessage(3, cm)
-    #sleep(0.1) #perhaps unnecessary
-    I2CMessage(5,0)
-    pass
+  I2CMessage(3, cm)
+  #sleep(0.1) # Perhaps unnecessary
+  I2CMessage(5,0)
 
+################################################################################
+## OpenCV Camera Actions and State Machine
 
-## OpenCV Camera Instantiation (Prelim)
-# Needs tweaking for different possible camera setups
 class state(Enum):
   IDLE = 0
   SCAN = 1
@@ -99,12 +95,11 @@ class state(Enum):
   MOVE = 4
 
 camState = state.IDLE
-#adjCount = 0
 cap = cv.VideoCapture(-1)
 if not cap.isOpened():
   print('Err: cannot open camera\n')
   exit()
-print('Opening video feed...')
+print('Starting marker tracking...\n')
 
 # Setup for ArUco parameters and marker dictionary
 arucoDict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -118,44 +113,48 @@ while True:
   # Image transformations for the detectMarkers function
   grayed = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
   corners, markIDs, rejected = aruco.detectMarkers(grayed, arucoDict, parameters = params)
-
-  # if busy
-  #continue
-  if (GPIO.input(4) == 0):
-    #print("busy")
-    continue
   
-  if(camState == state.IDLE): # Idle
-    print('Moving to SCAN') # Needs a short bit of code for idle state
-    if(markIDs is not None):
+  if(GPIO.input(4) == 0): # If the Arduino is busy/moving, skip loop actions
+    #print("busy")
+    if(camState == state.MOVE):
+      break
+    continue
+
+  
+  ## Pi-Side State Machine
+  if(camState == state.IDLE):
+    print('Moving to SCAN\n')
+    if(markIDs is not None): # If marker in frame on startup, skip SCAN state
       camState = state.ADJUST
+      print('Moving to ADJUST')
+      #cv.imshow('Initial State', aruco.drawDetectedMarkers(grayed, corners))
       continue
+    # Send signal to Arduino to initiate SCAN state
     I2CMessage(0,0)
     I2CMessage(1,0)
     camState = state.SCAN
-    # send I2C scan signal
-  elif(camState == state.SCAN): # Scan
-    if(markIDs is not None):
+  
+  elif(camState == state.SCAN):
+    if(markIDs is not None): # If a marker is detected, move to the ADJUST state
       camState = state.ADJUST
       print('Moving to ADJUST')
-      # Send stop command to robot
-      I2CMessage(0,0)
-      # Small delay signal to account for robot stop delay
-  elif(camState == state.ADJUST and markIDs is not None): # Adjust (Camera Angle)
-    camState, angle = angleMeas(markIDs[0], corners[0][0])
-    if(camState == state.ADJUST): # If the program sent back to adjust
-      rotate(-1*angle)
-      # Send angle to robot over I2C
-    if(adjFail):
+      I2CMessage(0,0) # Send stop command to Arduino for ADJUST state
+      
+  elif(camState == state.ADJUST and markIDs is not None):
+    camState, angle = angleMeas(markIDs[0], corners[0][0]) # Evaluate marker angle w/ camera
+    if(camState == state.ADJUST): # If the angle is above the set threshold
+      rotate(-1*angle) # Send angle to robot over I2C
+    if(adjFail): # If the robot fails to turn the required angle, continue
       camState = state.DISTANCE
       print('Busted!')
-  elif(camState == state.DISTANCE and markIDs is not None): # Distance
-    dist = distMeas(markIDs[0], corners[0][0])
-    # Send distance to Arduino
-    moveForward(dist * 30.48)
+      
+  elif(camState == state.DISTANCE and markIDs is not None):
+    dist = distMeas(markIDs[0], corners[0][0]) # Evaluate marker distance w/ camera
+    moveForward(dist * 30.48) # Send distance to Arduino
     camState = state.MOVE
-    print('Moving to MOVE')
-  else: # Move
+    print('Moving to MOVE\n')
+    
+  else: # MOVE state
     pass
   
 print('Done.\n')
